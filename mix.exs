83af026f7f50eb96e5b28f1bc6fee3719d53c8f5
Joseph Lozano <joseph@hey.com>
2024-11-23 00:00:00 -0500

    fly launch

--- mix.exs
+++ mix.exs
@@ -61,7 +61,9 @@ defmodule Roundest.MixProject do
       setup: ["deps.get", "cmd npm install --prefix assets", "ecto.setup"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
+      "assets.build": ["cmd npm run build --prefix assets"],
+      "assets.deploy": ["assets.build", "phx.digest"]
     ]
   end
 end
